e5553e061c06006dd44e285278d095e1622b401c
t4e1 <jtw5738@naver.com>
2025-09-20 23:33:54 +0900

    get post info handler & handler test

--- backend/internal/handlers/post.go
+++ backend/internal/handlers/post.go
@@ -15,6 +15,7 @@ import (
 
 type Handler interface {
 	GetPosts(c *gin.Context)
+	GetPost(c *gin.Context)
 }
 
 type handlerImpl struct {
@@ -31,12 +32,10 @@ func PingHandler(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
 
+// 등록일 기준 최근 50개를 호출하는 함수 (페이지 로딩시 자동 표시)
 func (h *handlerImpl) GetPosts(c *gin.Context) {
 
-	reqId := c.GetHeader("X-Request-ID")
-	if reqId == "" {
-		reqId = fmt.Sprint(utils.GenerateRequestId())
-	}
+	reqId := reqIdChecker(c)
 
 	// use gin.Contexts -> can check user connection & middleware information
 	ctx, cancle := context.WithTimeout(c.Request.Context(), 10*time.Second)
@@ -44,6 +43,36 @@ func (h *handlerImpl) GetPosts(c *gin.Context) {
 	defer cancle()
 
 	result, err := h.postSvc.GetPosts(ctx)
+	commonErrorChecker(c, ctx, err, reqId)
+	c.JSON(http.StatusOK, result)
+}
+
+// 특정 GIS 데이터를 선택하거나, 최근 등록글을 선택하면 해당 투고에 대한 상세 정보를 제공해주는 함수
+func (h *handlerImpl) GetPostInfo(c *gin.Context) {
+	reqId := reqIdChecker(c)
+
+	ctx, cancle := context.WithTimeout(c.Request.Context(), 10*time.Second)
+	ctx = context.WithValue(ctx, "requestId", reqId)
+	defer cancle()
+
+	result, err := h.postSvc.GetPostInfo(ctx)
+	commonErrorChecker(c, ctx, err, reqId)
+
+	c.JSON(http.StatusOK, result)
+}
+
+// request id 관련 로직을 통합해서 처리해주는 함수
+func reqIdChecker(c *gin.Context) string {
+	reqId := c.GetHeader("X-Request-ID")
+	if reqId == "" {
+		reqId = fmt.Sprint(utils.GenerateRequestId())
+	}
+
+	return reqId
+}
+
+// 공통적으로 발생할 수 있는 http response 관련 에러를 처리하는 함수
+func commonErrorChecker(c *gin.Context, ctx context.Context, err error, reqId string) {
 	if err != nil {
 		log.Printf("GetPostList failed - Request Id: %s, Error, %v", reqId, err)
 
@@ -69,5 +98,5 @@ func (h *handlerImpl) GetPosts(c *gin.Context) {
 		})
 		return
 	}
-	c.JSON(http.StatusOK, result)
+
 }
--- backend/internal/handlers/post_test.go
+++ backend/internal/handlers/post_test.go
@@ -18,13 +18,16 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+var testUUID uuid.UUID
+var testTime time.Time = time.Now()
+var testLocationText []byte = []byte("testlocation")
+var otherErrors error = fmt.Errorf("Someting goes wrong except ctx.Deadline and ctx.Canceled.")
+var sampleText string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+
 func TestGetPosts(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
 	testUUID, _ := uuid.NewUUID()
-	testTime := time.Now()
-	testLocationText := []byte("testlocation")
-	otherErrors := fmt.Errorf("Someting goes wrong except ctx.Deadline and ctx.Canceled.")
 
 	testCases := []struct {
 		name           string
@@ -36,14 +39,14 @@ func TestGetPosts(t *testing.T) {
 		{
 			name: "Success - Formal Response",
 			svcReturn: []models.Posts{
-				{PostId: testUUID, PostedDate: testTime, Latitude: 0.01, Longtitude: 0.02, AddressText: "test", Location: testLocationText},
-				{PostId: testUUID, PostedDate: testTime, Latitude: 0.02, Longtitude: 0.01, AddressText: "test2", Location: testLocationText},
+				{PostId: testUUID, PostedDate: testTime, Latitude: 0.01, Longtitude: 0.02, AddressText: sampleText, Location: testLocationText},
+				{PostId: testUUID, PostedDate: testTime, Latitude: 0.02, Longtitude: 0.01, AddressText: sampleText, Location: testLocationText},
 			},
 			returnErr:      nil,
 			expectedStatus: http.StatusOK,
 			expectedBody: []models.Posts{
-				{PostId: testUUID, PostedDate: testTime, Latitude: 0.01, Longtitude: 0.02, AddressText: "test", Location: testLocationText},
-				{PostId: testUUID, PostedDate: testTime, Latitude: 0.02, Longtitude: 0.01, AddressText: "test2", Location: testLocationText},
+				{PostId: testUUID, PostedDate: testTime, Latitude: 0.01, Longtitude: 0.02, AddressText: sampleText, Location: testLocationText},
+				{PostId: testUUID, PostedDate: testTime, Latitude: 0.02, Longtitude: 0.01, AddressText: sampleText, Location: testLocationText},
 			},
 		},
 		{
@@ -87,6 +90,9 @@ func TestGetPosts(t *testing.T) {
 			handler.GetPosts(c)
 
 			if tc.name == "Success - Formal Response" {
+
+				// http response 의 body는 []byte 형태의 json 문자열이 들어가 있음.
+				// 이를 unmarshal 해서 []models.Posts 형태로 변환한 다음 비교를 진행
 				var got []models.Posts
 				if err := json.Unmarshal(w.Body.Bytes(), &got); err != nil {
 					t.Fatalf("failed to unmarshal response body: %v", err)
@@ -115,6 +121,125 @@ func TestGetPosts(t *testing.T) {
 					if !bytes.Equal(got[i].Location, tc.expectedBody[i].Location) {
 						t.Errorf("location mismatch: expected %v, got %v", tc.expectedBody[i].Location, got[i].Location)
 					}
+				}
+			}
+		})
+	}
+}
+
+func TestGetPostInfo(t *testing.T) {
+
+	testUUID, _ := uuid.NewUUID()
+
+	testCases := []struct {
+		name           string
+		svcReturn      *models.Post
+		returnErr      error
+		expectedStatus int
+		expectedBody   models.Post
+	}{
+		{
+			name: "Success - Formal Result(GetPostsInfo)",
+			svcReturn: &models.Post{
+				PostId:       testUUID,
+				Content:      "",
+				IncidentDate: testTime,
+				PostedDate:   testTime,
+				Latitude:     0.01,
+				Longtitude:   0.02,
+				AddressText:  sampleText,
+				Location:     testLocationText,
+				IsActive:     true,
+			},
+			returnErr:      nil,
+			expectedStatus: http.StatusOK,
+			expectedBody: models.Post{
+				PostId:       testUUID,
+				Content:      "",
+				IncidentDate: testTime,
+				PostedDate:   testTime,
+				Latitude:     0.01,
+				Longtitude:   0.02,
+				AddressText:  sampleText,
+				Location:     testLocationText,
+				IsActive:     true,
+			},
+		},
+
+		{
+			name:           "Error - Context RequestTimeout(GetPostInfo)",
+			svcReturn:      nil,
+			returnErr:      context.DeadlineExceeded,
+			expectedStatus: http.StatusRequestTimeout,
+			expectedBody:   models.Post{},
+		},
+		{
+			name:           "Error - Context Canceled(GetPostInfo)",
+			svcReturn:      nil,
+			returnErr:      context.Canceled,
+			expectedStatus: http.StatusRequestTimeout,
+			expectedBody:   models.Post{},
+		},
+		{
+			name:           "Error - InternalServerError(GetPostInfo)",
+			svcReturn:      nil,
+			returnErr:      otherErrors,
+			expectedStatus: http.StatusInternalServerError,
+			expectedBody:   models.Post{},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockSvc := services.NewMockPostService(t)
+			mockSvc.On("GetPost", mock.Anything).Return(tc.svcReturn, tc.returnErr)
+			url := fmt.Sprintf("/post/%s", testUUID)
+
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+			req, _ := http.NewRequest(http.MethodGet, url, nil)
+			req.Header.Set("X-Request-ID", "test-request-id")
+
+			c.Request = req
+
+			handler := handlers.NewPostHandler(mockSvc)
+			handler.GetPosts(c)
+
+			if tc.name == "Success - Formal Result(GetPostsInfo)" {
+				var got models.Post
+
+				if err := json.Unmarshal(w.Body.Bytes(), &got); err != nil {
+					t.Fatalf("failed to unmarshal response body: %v", err)
+				}
+				if got.PostId != tc.expectedBody.PostId {
+					t.Errorf("postId mismatch: expected %v, got %v", tc.expectedBody.PostId, got.PostId)
+				}
+				if !got.PostedDate.Equal(tc.expectedBody.PostedDate) {
+					t.Errorf("postedDate mismatch: expected %v, got %v", tc.expectedBody.PostedDate, got.PostedDate)
+				}
+				if got.Latitude != tc.expectedBody.Latitude {
+					t.Errorf("latitude mismatch: expected %v, got %v", tc.expectedBody.Latitude, got.Latitude)
+				}
+				if got.Longtitude != tc.expectedBody.Longtitude {
+					t.Errorf("longtitude mismatch: expected %v, got %v", tc.expectedBody.Longtitude, got.Longtitude)
+				}
+				if got.AddressText != tc.expectedBody.AddressText {
+					t.Errorf("addressText mismatch: expected %v, got %v", tc.expectedBody.AddressText, got.AddressText)
+				}
+				if !bytes.Equal(got.Location, tc.expectedBody.Location) {
+					t.Errorf("location mismatch: expected %v, got %v", tc.expectedBody.Location, got.Location)
+				}
+				if got.Content != tc.expectedBody.Content {
+					t.Errorf("content mismatch: expected %v, got %v", tc.expectedBody.Content, got.Content)
+				}
+				if !got.IncidentDate.Equal(tc.expectedBody.IncidentDate) {
+					t.Errorf("incidentDate mismatch: expected %v, got %v", tc.expectedBody.IncidentDate, got.IncidentDate)
+				}
+				if got.IsActive != tc.expectedBody.IsActive {
+					t.Errorf("isActive mismatch: expected %v, got %v", tc.expectedBody.IsActive, got.IsActive)
 				}
 			}
 		})
